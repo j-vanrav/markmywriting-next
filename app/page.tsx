291fbf65ebc8dda3182f35acb90356410ad9d1c6
j-vanrav <j.vanrav2@gmail.com>
2024-07-07 17:49:31 +0800

    capacitor

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <div className="flex flex-col items-center gap-2">hello world</div>;
+  return (
+    <div className="flex flex-col items-center gap-2">
+      hello world<div className="size-5 bg-destructive-foreground">test</div>
+    </div>
+  );
 }
---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "app.jvr.markmywriting",
+  appName: "MarkMyWriting",
+  webDir: "out",
+  // live reloading
+  // server: {
+  //   url: "http://192.168.86.23:3000",
+  //   cleartext: true,
+  // },
+};
+
+export default config;
