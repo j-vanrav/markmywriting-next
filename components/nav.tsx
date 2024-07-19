afc64352d72e6003b02e7b3ff41e4721943a1866
j-vanrav <j.vanrav2@gmail.com>
2024-07-19 19:35:57 +0800

    framer motion nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,6 +3,8 @@ import { cn } from "@/lib/utils";
 import { Camera, NotebookPen, User } from "lucide-react";
 import React, { useState } from "react";
 import { HapticsClick } from "@/lib/client-utils";
+import TapButton from "./tap-button";
+import { LayoutGroup } from "framer-motion";
 
 function NavButton({
   selectedPage,
@@ -13,62 +15,68 @@ function NavButton({
   name: PageName;
   trigger: (name: PageName) => void;
 }) {
-  const [pressed, setPressed] = useState(false);
   return (
-    <button
+    <TapButton
       className={cn(
-        "rounded-full size-16 transition-all duration-200 flex flex-row justify-center items-center",
-        selectedPage === name && "w-48",
+        "rounded-full h-16 min-w-16 flex flex-row justify-center items-center",
+        // selectedPage === name && "w-48",
         name === "review" && "bg-nbgreen",
         name === "compose" && "bg-nborange",
-        name === "profile" && "bg-nbpurple",
-        pressed && "scale-75"
+        name === "profile" && "bg-nbpurple"
       )}
-      onPointerDown={() => setPressed(true)}
-      onPointerUp={() => setPressed(false)}
-      onPointerLeave={() => setPressed(false)}
-      onTouchStart={() => setPressed(true)}
-      onTouchCancel={() => setPressed(false)}
-      onTouchMove={() => setPressed(false)}
       onClick={() => {
         HapticsClick();
         trigger(name);
       }}
+      layout
+      style={{
+        width: selectedPage === name ? 192 : 64,
+      }}
+      transition={{ damping: 30, stiffness: 600 }}
     >
-      {name === "review" && (
-        <NotebookPen
-          absoluteStrokeWidth
-          strokeWidth={1.5}
-          className={cn(
-            "size-8 min-w-8 transition-all duration-200",
-            selectedPage === name ? "size-6 min-w-6" : "ml-4"
-          )}
-        />
-      )}
-      {name === "compose" && (
-        <Camera
-          absoluteStrokeWidth
-          strokeWidth={1.5}
-          className={cn(
-            "size-8 min-w-8 transition-all duration-200",
-            selectedPage === name ? "size-6 min-w-6" : "ml-4"
-          )}
-        />
-      )}
-      {name === "profile" && (
-        <User
-          absoluteStrokeWidth
-          strokeWidth={1.5}
-          className={cn(
-            "size-8 min-w-8 transition-all duration-200",
-            selectedPage === name ? "size-6 min-w-6" : "ml-4"
-          )}
-        />
-      )}
+      {name === "review" &&
+        (selectedPage !== name ? (
+          <NotebookPen
+            absoluteStrokeWidth
+            strokeWidth={1.5}
+            className={cn(
+              "size-8 min-w-8"
+              // selectedPage === name ? "size-6 min-w-6" : "ml-4"
+            )}
+          />
+        ) : (
+          <span className="font-medium text-2xl">Review</span>
+        ))}
+      {name === "compose" &&
+        (selectedPage !== name ? (
+          <Camera
+            absoluteStrokeWidth
+            strokeWidth={1.5}
+            className={cn(
+              "size-8 min-w-8"
+              // selectedPage === name ? "size-6 min-w-6" : "ml-4"
+            )}
+          />
+        ) : (
+          <span className="font-medium text-2xl">Compose</span>
+        ))}
+      {name === "profile" &&
+        (selectedPage !== name ? (
+          <User
+            absoluteStrokeWidth
+            strokeWidth={1.5}
+            className={cn(
+              "size-8 min-w-8"
+              // selectedPage === name ? "size-6 min-w-6" : "ml-4"
+            )}
+          />
+        ) : (
+          <span className="font-medium text-2xl">Profile</span>
+        ))}
       <div className={cn(selectedPage === name ? "w-2" : "w-0")} />
-      <div
+      {/* <div
         className={cn(
-          "font-normal text-xl opacity-0 overflow-hidden transition-all duration-200 flex flex-row justify-center",
+          "font-normal text-xl opacity-0 overflow-hidden transition-opacity duration-200 flex flex-row justify-center",
           selectedPage === name && "w-fit opacity-100"
         )}
       >
@@ -77,8 +85,8 @@ function NavButton({
           {name === "compose" && "Compose"}
           {name === "profile" && "Profile"}
         </span>
-      </div>
-    </button>
+      </div> */}
+    </TapButton>
   );
 }
 
@@ -92,21 +100,23 @@ export default function Nav({
   return (
     <nav className="fixed bottom-0 w-full p-2 px-4 flex flex-row justify-center z-50">
       <div className="rounded-full bg-black p-1 gap-1 flex flex-row text-black w-full justify-between">
-        <NavButton
-          selectedPage={selectedPage}
-          name={"review"}
-          trigger={setSelectedPage}
-        />
-        <NavButton
-          selectedPage={selectedPage}
-          name={"compose"}
-          trigger={setSelectedPage}
-        />
-        <NavButton
-          selectedPage={selectedPage}
-          name={"profile"}
-          trigger={setSelectedPage}
-        />
+        <LayoutGroup>
+          <NavButton
+            selectedPage={selectedPage}
+            name={"review"}
+            trigger={setSelectedPage}
+          />
+          <NavButton
+            selectedPage={selectedPage}
+            name={"compose"}
+            trigger={setSelectedPage}
+          />
+          <NavButton
+            selectedPage={selectedPage}
+            name={"profile"}
+            trigger={setSelectedPage}
+          />
+        </LayoutGroup>
       </div>
     </nav>
   );
--- components/tap-button.tsx
+++ components/tap-button.tsx
@@ -7,6 +7,9 @@ export default function TapButton({
   onClick,
   className,
   children,
+  whileHover = { scale: 1.2 },
+  whileTap = { scale: 0.9 },
+  transition,
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & HTMLMotionProps<"button">) {
   return (
@@ -18,9 +21,14 @@ export default function TapButton({
           onClick(e);
         }
       }}
-      whileHover={{ scale: 1.2 }}
-      whileTap={{ scale: 0.9 }}
-      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      whileHover={whileHover}
+      whileTap={whileTap}
+      transition={{
+        type: "spring",
+        stiffness: 400,
+        damping: 17,
+        ...transition,
+      }}
       {...props}
     >
       {children}
